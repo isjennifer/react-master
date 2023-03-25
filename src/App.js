ddf3d4df1db6f7ab78a8aea92a7211896bbda623
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-25 15:39:33 +0900

    How to use styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import styled from "styled-components";
 
+const Father = styled.div`
+  display : flex;
+`;
+
+const BoxOne = styled.div`
+  background-Color : teal;
+  width : 100px;
+  height : 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-Color : black;
+  width : 100px;
+  height : 100px;
+`;
+
+const Text = styled.span`
+  color : white;
+`;
+
 function App() {
   return (
-    <div style={{display:"flex"}}>
-      <div style={{backgroundColor:"teal", width:100, height:100}}></div>
-      <div style={{backgroundColor:"tomato", width:100, height:100}}></div>
-    </div>
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo />
+    </Father>
   );
 }
 
