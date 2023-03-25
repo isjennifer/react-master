78f4c06993e58daaec92003b35c2408205dd24ab
김성하 <dundun@gimseonghaui-MacBookAir.local>
2023-03-25 17:05:04 +0900

    styled-components: 하위컴포 타겟팅

--- src/App.js
+++ src/App.js
@@ -9,12 +9,11 @@ const rotateAnimation = keyframes`
     transform : rotate(0deg);
   }
   50% {
-    border-radius : 100px;
-    transform:rotate(360deg);
+    transform:rotate(180deg);
   }
   100% {
     border-radius : 0px;
-    transform : rotate(0deg);
+    transform : rotate(360deg);
   }
 `;
 
@@ -22,13 +21,27 @@ const Box = styled.div`
   background-color : tomato;
   width : 200px;
   height : 200px;
-  animation : ${rotateAnimation} 1s linear infinite
+  display : flex;
+  justify-content : center;
+  align-items : center;
+  animation : ${rotateAnimation} 1s linear infinite;
+  span {
+    font-size: 40px;
+    &:hover {
+      font-size: 60px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  };
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box>
+        <span>😀</span>
+      </Box>
     </Wrapper>
   );
 }
